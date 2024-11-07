ccd4ce173251ee36dddb457726f6c97251534085
Iskandar <dexqonoviskandar104@gmail.com>
2024-11-07 23:06:40 +0500

    telefon raqamni qabul qildim

--- 5-topshiriq.py
+++ 5-topshiriq.py
@@ -91,3 +91,64 @@ while True:
     form.ask_email()
 
     form.show_user_info()
+
+
+    class UserForm:
+        def __init__(self):
+            self.first_name = ""
+            self.last_name = ""
+            self.age = 0
+            self.email = ""
+            self.phone_number = ""
+
+        def ask_first_name(self):
+            self.first_name = input("Ismingizni kiriting: ")
+
+        def ask_last_name(self):
+            self.last_name = input("Familiyangizni kiriting: ")
+
+        def ask_age(self):
+            while True:
+                try:
+                    self.age = int(input("Yoshingizni kiriting: "))
+                    if self.age <= 0:
+                        print("Yosh musbat bo'lishi kerak. Iltimos, yana bir bor urinib ko'ring.")
+                        continue
+                    break
+                except ValueError:
+                    print("Iltimos, to'g'ri yosh kiriting (raqam sifatida).")
+
+        def ask_email(self):
+            while True:
+                self.email = input("Elektron pochta manzilingizni kiriting: ")
+                if "@" in self.email and "." in self.email:
+                    break
+                else:
+                    print("Iltimos, to'g'ri elektron pochta manzilini kiriting.")
+
+        def ask_phone_number(self):
+            while True:
+                self.phone_number = input("Telefon raqamingizni kiriting (masalan, +998 ...): ")
+                if self.phone_number.startswith("+998") and len(self.phone_number) == 13:
+                    break
+                else:
+                    print("Iltimos, telefon raqamingizni to'g'ri kiriting (masalan, +998 ...).")
+
+        def show_user_info(self):
+            print("\nFoydalanuvchi ma'lumotlari:")
+            print(f"Ism: {self.first_name}")
+            print(f"Familiya: {self.last_name}")
+            print(f"Yosh: {self.age}")
+            print(f"Elektron pochta: {self.email}")
+            print(f"Telefon raqam: {self.phone_number}")
+
+
+    form = UserForm()
+
+    form.ask_first_name()
+    form.ask_last_name()
+    form.ask_age()
+    form.ask_email()
+    form.ask_phone_number()
+
+    form.show_user_info()
