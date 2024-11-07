b92d984e793cb6b4ea5da6b7860230ac5da84db7
Iskandar <dexqonoviskandar104@gmail.com>
2024-11-07 22:11:06 +0500

    Ismni qabul qildim

---
+++ 5-topshiriq.py
@@ -0,0 +1,43 @@
+while True:
+    class UserForm:
+        def __init__(self):
+            self.name = ""
+            self.age = 0
+            self.email = ""
+
+        def ask_name(self):
+            self.name = input("Ismingizni kiriting: ")
+
+        def ask_age(self):
+            while True:
+                try:
+                    self.age = int(input("Yoshingizni kiriting: "))
+                    if self.age <= 0:
+                        print("Yosh musbat bo'lishi kerak. Iltimos, yana bir bor urinib ko'ring.")
+                        continue
+                    break
+                except ValueError:
+                    print("Iltimos, to'g'ri yosh kiriting (raqam sifatida).")
+
+        def ask_email(self):
+            while True:
+                self.email = input("Elektron pochta manzilingizni kiriting: ")
+                if "@" in self.email and "." in self.email:
+                    break
+                else:
+                    print("Iltimos, to'g'ri elektron pochta manzilini kiriting.")
+
+        def show_user_info(self):
+            print("\nFoydalanuvchi ma'lumotlari:")
+            print(f"Ism: {self.name}")
+            print(f"Yosh: {self.age}")
+            print(f"Elektron pochta: {self.email}")
+
+
+    form = UserForm()
+
+    form.ask_name()
+    form.ask_age()
+    form.ask_email()
+
+    form.show_user_info()
